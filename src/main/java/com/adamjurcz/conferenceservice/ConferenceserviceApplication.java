67ca4d2d50bf5c3a235478f27119b5ba05206daf
AdamJurcz <adamos00@op.pl>
2023-05-13 20:50:13 +0200

    Initial commit

---
+++ src/main/java/com/adamjurcz/conferenceservice/ConferenceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adamjurcz.conferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adamjurcz/conferenceservice/ConferenceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamjurcz.conferenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
