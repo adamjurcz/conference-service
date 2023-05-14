d4fb003a7d72cc435e24137ab8270f86d556907b
AdamJurcz <adamos00@op.pl>
2023-05-14 20:51:55 +0200

    Changes to lecture and user profile models

--- src/main/java/com/adamjurcz/conferenceservice/core/domain/Lecture.java
+++ src/main/java/com/adamjurcz/conferenceservice/core/domain/Lecture.java
@@ -14,5 +14,11 @@ public class Lecture {
     private Identity id;
     private String main_subject;
     private Date start_time;
+    private Integer path_num;
+    private Integer max_listeners;
     private List<UserProfile> listeners;
+
+    public boolean hasMaximumListeners(){
+        return !(listeners.size() <= 5);
+    }
 }
--- src/main/java/com/adamjurcz/conferenceservice/core/domain/UserProfile.java
+++ src/main/java/com/adamjurcz/conferenceservice/core/domain/UserProfile.java
@@ -2,6 +2,7 @@ package com.adamjurcz.conferenceservice.core.domain;
 
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -14,5 +15,16 @@ public class UserProfile {
     private String login;
     private String email;
     private List<Lecture> lectures;
+
+    public static UserProfile newInstance(String login, String email){
+        return new UserProfile(Identity.nothing(), login,
+                email, new ArrayList<>());
+    }
+
+    public boolean isHourAlreadyReserved(Lecture lecture){
+        return lectures.stream()
+                .anyMatch(lecture_member ->
+                        lecture_member.getStart_time().equals(lecture.getStart_time()));
+    }
 
 }
