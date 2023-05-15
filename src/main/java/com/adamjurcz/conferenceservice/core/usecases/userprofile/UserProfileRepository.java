69b54e1bb27684442e6a390efe3094ab4145521e
AdamJurcz <adamos00@op.pl>
2023-05-15 16:00:03 +0200

    Added get all declaration to user interface

--- src/main/java/com/adamjurcz/conferenceservice/core/usecases/userprofile/UserProfileRepository.java
+++ src/main/java/com/adamjurcz/conferenceservice/core/usecases/userprofile/UserProfileRepository.java
@@ -3,9 +3,12 @@ package com.adamjurcz.conferenceservice.core.usecases.userprofile;
 import com.adamjurcz.conferenceservice.core.domain.Identity;
 import com.adamjurcz.conferenceservice.core.domain.UserProfile;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserProfileRepository {
+    List<UserProfile> getAll();
+
     Optional<UserProfile> getByLogin(String string);
 
     UserProfile persist(UserProfile userProfile);
