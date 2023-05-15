726f0f5d1a9ee2ed5eb896f2f47d5c29abb22b28
AdamJurcz <adamos00@op.pl>
2023-05-15 11:51:52 +0200

    Implemented delete lecture reservation use case

---
+++ src/main/java/com/adamjurcz/conferenceservice/core/usecases/lecture/DeleteLectureReservationUseCase.java
@@ -0,0 +1,45 @@
+package com.adamjurcz.conferenceservice.core.usecases.lecture;
+
+import com.adamjurcz.conferenceservice.core.domain.Identity;
+import com.adamjurcz.conferenceservice.core.domain.Lecture;
+import com.adamjurcz.conferenceservice.core.domain.UserProfile;
+import com.adamjurcz.conferenceservice.core.exceptions.NotFoundException;
+import com.adamjurcz.conferenceservice.core.usecases.UseCase;
+import com.adamjurcz.conferenceservice.core.usecases.userprofile.UserProfileRepository;
+import lombok.Value;
+
+public class DeleteLectureReservationUseCase extends UseCase<DeleteLectureReservationUseCase.Input, DeleteLectureReservationUseCase.Output> {
+    private LectureRepository lectureRepository;
+    private UserProfileRepository userProfileRepository;
+
+    public DeleteLectureReservationUseCase(LectureRepository lectureRepository, UserProfileRepository userProfileRepository) {
+        this.lectureRepository = lectureRepository;
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    @Override
+    public Output execute(Input input) {
+        UserProfile userProfile = userProfileRepository.getByLogin(input.getLogin())
+                .orElseThrow(()->new NotFoundException("Login: %s nie istnieje"));
+
+        Lecture userLecture = userProfile.getLectures()
+                .stream()
+                .filter(lecture -> lecture.getId().equals(input.getLecture_id()))
+                .findFirst()
+                .orElseThrow(()->new NotFoundException("Prelekcja o id: %s nie istnieje!", input.getLecture_id().getValue()));
+        userProfile.getLectures().remove(userLecture);
+
+        userProfileRepository.persist(userProfile);
+        return new Output();
+    }
+
+    @Value
+    public static class Input implements UseCase.Input{
+        String login;
+        Identity lecture_id;
+    }
+
+    @Value
+    public static class Output implements UseCase.Output{
+    }
+}
