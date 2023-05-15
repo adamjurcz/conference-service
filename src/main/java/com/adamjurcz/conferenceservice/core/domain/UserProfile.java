14c5d6bfda6b7c3728b1594ca43b0eab1ecf1e31
AdamJurcz <adamos00@op.pl>
2023-05-15 14:56:40 +0200

    Added email validation to user profile class

--- src/main/java/com/adamjurcz/conferenceservice/core/domain/UserProfile.java
+++ src/main/java/com/adamjurcz/conferenceservice/core/domain/UserProfile.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,6 +20,14 @@ public class UserProfile {
     public static UserProfile newInstance(String login, String email){
         return new UserProfile(Identity.nothing(), login,
                 email, new ArrayList<>());
+    }
+
+    public static boolean isEmailValid(String email){
+        return ((Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+                .matcher(email)
+                .matches()) &&
+                email.length()<60
+                );
     }
 
     public boolean isHourAlreadyReserved(Lecture lecture){
