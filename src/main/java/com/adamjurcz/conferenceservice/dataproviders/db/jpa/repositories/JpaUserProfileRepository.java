47a9b85c3792f7e5fcd7112b60c5bd0260e63bc7
AdamJurcz <adamos00@op.pl>
2023-05-19 21:01:22 +0200

    Added correct user fetching to jpa repository

--- src/main/java/com/adamjurcz/conferenceservice/dataproviders/db/jpa/repositories/JpaUserProfileRepository.java
+++ src/main/java/com/adamjurcz/conferenceservice/dataproviders/db/jpa/repositories/JpaUserProfileRepository.java
@@ -1,8 +1,16 @@
 package com.adamjurcz.conferenceservice.dataproviders.db.jpa.repositories;
 
 import com.adamjurcz.conferenceservice.dataproviders.db.jpa.entities.UserProfileData;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserProfileRepository extends JpaRepository<UserProfileData, Integer> {
+
+    @EntityGraph(attributePaths = "lectures")
+    Optional<UserProfileData> findByLogin(String login);
+
+    boolean existsByEmail(String email);
 
 }
