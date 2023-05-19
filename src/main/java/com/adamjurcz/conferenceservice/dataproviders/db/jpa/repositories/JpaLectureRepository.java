a31b2e3a673e425d2f51b544a66fe50f67dccbe8
AdamJurcz <adamos00@op.pl>
2023-05-19 21:06:45 +0200

    Added correct findAll data fetching from jpa

--- src/main/java/com/adamjurcz/conferenceservice/dataproviders/db/jpa/repositories/JpaLectureRepository.java
+++ src/main/java/com/adamjurcz/conferenceservice/dataproviders/db/jpa/repositories/JpaLectureRepository.java
@@ -1,7 +1,12 @@
 package com.adamjurcz.conferenceservice.dataproviders.db.jpa.repositories;
 
 import com.adamjurcz.conferenceservice.dataproviders.db.jpa.entities.LectureData;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaLectureRepository extends JpaRepository<LectureData, Integer> {
+    @EntityGraph(attributePaths = "listeners")
+    List<LectureData> findAll();
 }
